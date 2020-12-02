ddbda2cdf5f5b5a69dae4b6dcb48d197ec93db84
gadeyka <gadeyka@gmail.com>
2020-12-02 11:45:59 -0800

    sleep 2000 // System.SetProperty

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -10,7 +10,7 @@ public class SimpleTest {
     @Test
     public void simpleTest() throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "d:/Work/Projects/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "d:/Work/Projects/chromedriver.exe");
 
         WebDriver browser = new ChromeDriver();
         browser.get("https://github.com/SergeiDemyanenko/PlatformaticaQA");
@@ -18,7 +18,7 @@ public class SimpleTest {
 
         Assert.assertEquals(name.getText(), "PlatformaticaQA");
 
-        Thread.sleep(3000);
+        Thread.sleep(2000);
         browser.close();
     }
 }
