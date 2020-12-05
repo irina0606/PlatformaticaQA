0854b45be17785f2e1046e92a78e357a74368afc
SVar25 <varakuta3112@gmail.com>
2020-12-05 08:52:09 -0600

    added new element

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -31,5 +31,32 @@ public class SimpleTest extends BaseTest {
         WebElement input = driver.findElement(By.xpath("(//div[@class='input-group']/input)[1]"));
         Assert.assertEquals(input.getAttribute("value"), "https://github.com/SergeiDemyanenko/PlatformaticaQA.git");
     }
+    @Test
+    public void newTest() throws InterruptedException {
 
+        WebDriver driver = getDriver();
+        driver.get("https://github.com/SergeiDemyanenko/PlatformaticaQA");
+
+        WebElement button = driver.findElement(By.id("branch-select-menu"));
+        button.click();
+
+        Thread.sleep(2000);
+
+        WebElement link = driver.findElement(By.xpath("//footer/a[contains(text(), 'branches')]"));
+        link.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://github.com/SergeiDemyanenko/PlatformaticaQA/branches");
+    }
+
+    @Test
+    public void fifthTest () {
+
+        WebDriver driver = getDriver();
+        driver.get("https://www.smithsonianmag.com/history/thanksgiving-myth-and-what-we-should-be-teaching-kids-180973655/");
+
+        WebElement name = driver.findElement(By.xpath("//div[3]//h1"));
+
+        Assert.assertEquals(name.getText(), "The Myths of the Thanksgiving Story and the Lasting Damage They Imbue");
+
+    }
 }
